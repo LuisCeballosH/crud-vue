d03096aef5b4a2c092818c85ef9818d59f696095
Luis Ceballos H <luisceballoshernandez@gmail.com>
2020-10-25 16:01:27 -0500

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,38 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    titleState: "test",
+    tareasState: [],
+    nuevaTareaState: "",
   },
   mutations: {
+    agregarMutations(state) {
+      state.tareasState.push({
+        name: state.nuevaTareaState,
+        state: true,
+      });
+      state.nuevaTareaState = "";
+      localStorage.setItem("data", JSON.stringify(state.tareasState));
+    },
+    editarMutations(state, index) {
+      state.tareasState[index].state = !state.tareasState[index].state;
+      localStorage.setItem("data", JSON.stringify(state.tareasState));
+    },
+    eliminarMutations(state, index) {
+      state.tareasState.splice(index, 1);
+      localStorage.setItem("data", JSON.stringify(state.tareasState));
+    },
+    loadData(state, actionData) {
+      console.log(actionData);
+      state.tareasState = actionData || [];
+    }
   },
   actions: {
+    getData({ commit }) {
+      console.log('test');
+      let datos = JSON.parse(localStorage.getItem("data"));
+      commit('loadData', datos);
+    }
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,95 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container my-5">
+      <h3>{{ titleState }}</h3>
+      <input
+        type="text"
+        class="form-control my-3"
+        v-model="$store.state.nuevaTareaState"
+        v-on:keyup.enter="agregarMutations"
+      />
+      <button
+        type="button"
+        class="btn btn-primary"
+        v-on:click="agregarMutations"
+      >
+        Primary
+      </button>
+      <div
+        class="alert alert-primary my-3 d-flex justify-content-between"
+        role="alert"
+        v-bind:class="{
+          'alert-success': item.state,
+          'alert-danger': !item.state,
+        }"
+        v-for="(item, index) of tareasState"
+        :key="index"
+      >
+        <div>{{ item.name }} - {{ item.state }}</div>
+        <div>
+          <button
+            type="button"
+            class="btn btn-danger btn-sm mx-1"
+            v-on:click="eliminarMutations(index)"
+          >
+            X
+          </button>
+          <button
+            type="button"
+            class="btn btn-success btn-sm mx-1"
+            v-on:click="editarMutations(index)"
+          >
+            OK
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { mapState, mapMutations, mapActions } from "vuex";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data() {
+    return {
+      title: "test",
+      tareas: [],
+      nuevaTarea: "",
+    };
+  },
+  computed: {
+    ...mapState(["titleState", "tareasState", "nuevaTareaState"]),
+  },
+  methods: {
+    agregar() {
+      this.tareas.push({
+        name: this.nuevaTarea,
+        state: true,
+      });
+      this.nuevaTarea = "";
+      localStorage.setItem("data", JSON.stringify(this.tareas));
+    },
+    editar(index) {
+      this.tareas[index].state = !this.tareas[index].state;
+      localStorage.setItem("data", JSON.stringify(this.tareas));
+    },
+    eliminar(index) {
+      this.tareas.splice(index, 1);
+      localStorage.setItem("data", JSON.stringify(this.tareas));
+    },
+    ...mapMutations([
+      "agregarMutations",
+      "editarMutations",
+      "eliminarMutations",
+    ]),
+    ...mapActions(["getData"]),
+  },
+  created() {
+    let datos = JSON.parse(localStorage.getItem("data"));
+    this.tareas = datos || [];
+    this.$store.dispatch('getData');
+  },
+};
 </script>
